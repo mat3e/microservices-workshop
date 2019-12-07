3d4873ccb6d1445c8c33bde105b6206d28102a9f
Mateusz Chrzonstowski <mateusz.chrzonstowski@allegro.pl>
2019-12-07 21:27:19 +0100

    Add missing answers

---
+++ cw11/odp/plans/src/main/java/io/github/mat3e/adapter/TaskController.java
@@ -0,0 +1,50 @@
+package io.github.mat3e.adapter;
+
+import io.github.mat3e.ConfigProps;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping("/api/plans/{planId}/tasks")
+class TaskController {
+    private ProjectPlanRepository repository;
+    private RestTemplate client;
+    private String tasksUrl;
+
+    public TaskController(ProjectPlanRepository repository, RestTemplate client, ConfigProps props) {
+        this.repository = repository;
+        this.tasksUrl = props.getServices().getTasksUrl();
+        this.client = client;
+    }
+
+    @PutMapping
+    ResponseEntity<List<String>> createTasksFromPlan(@PathVariable String planId) {
+        return repository.findById(planId)
+                .map(plan -> Stream.concat(
+                        Stream.of(new TaskDto(plan.getName(), plan.getDeadline())),
+                        plan.getProjectSteps().stream().map(
+                                planStep -> new TaskDto(
+                                        planStep.getDescription(),
+                                        plan.getDeadline()
+                                                .plusDays(planStep.getDaysToProjectDeadline())
+                                )
+                        )).map(this::sendTask)
+                        .map(TaskDto::getId)
+                        .collect(toList())
+                ).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    private TaskDto sendTask(TaskDto taskToSend) {
+        return client.postForObject(tasksUrl, taskToSend, TaskDto.class);
+    }
+}
